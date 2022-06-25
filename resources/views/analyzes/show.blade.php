adfa7f14271b119e99d6006b4e4d4dd0d2b9f44b
Даниил Хижняков <khizhnyakovdd@gmail.com>
2022-06-25 11:35:52 +0500

    delete added and date fix

--- resources/views/analyzes/show.blade.php
+++ resources/views/analyzes/show.blade.php
@@ -3,11 +3,19 @@
 @section('content')
     <div class="container-fluid content">
         <div class="m-3">ФИО: {{$analyze->patient->name}}</div>
-        <div class="m-3">Дата анализа: {{ date('d.m.Y', strtotime($analyze->patient->created_at))}}</div>
+        <div class="m-3">Дата анализа: {{ date('d.m.Y', strtotime($analyze->created_at))}}</div>
         <div class="m-3">Результат анализа: {{$analyze->predict_photo}}</div>
         <div class="m-3">Количество зубов с кариесом: {{$analyze->caries_count}}</div>
         <div class="m-3">Ссылка для скачивания: {{$analyze->predict_xml}}</div>
-        <button class="btn btn-outline-danger m-3">Удалить запись</button>
+        @if ($analyze->user_id == Auth::id())
+            <form class="d-inline" action="{{ route('analyzes.destroy', $analyze->id) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-outline-danger m-3">Удалить запись</button>
+            </form>
+        @endif
+
+
     </div>
 
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,13 +40,19 @@
                 <a href="/analyzes/{{$analyze->id}}" class="list-group-item d-flex flex-column justify-content-end">
                     ФИО: {{ $analyze->patient->name }},
                     Дата рождения: {{ date('d.m.Y', strtotime($analyze->patient->date_of_birth))}},
-                    Дата анализа: {{ date('d.m.Y', strtotime($analyze->patient->created_at))}}
-                    <button class="btn btn-outline-danger btn-sm delete">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                        </svg>
-                    </button>
+                    Дата анализа: {{ date('d.m.Y', strtotime($analyze->created_at))}}
+                    @if ($analyze->user_id == Auth::id())
+                        <form class="d-inline" action="{{ route('analyzes.destroy', $analyze->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-outline-danger btn-sm delete">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                </svg>
+                            </button>
+                        </form>
+                    @endif
                 </a>
             @endforeach
             </div>
