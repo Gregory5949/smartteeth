7840d0fcdb32e6a7fc94a18ed3a855b3b19c262f
Даниил Хижняков <khizhnyakovdd@gmail.com>
2022-06-25 17:33:15 +0500

    change predict

--- resources/views/analyzes/show.blade.php
+++ resources/views/analyzes/show.blade.php
@@ -3,11 +3,12 @@
 @section('content')
     <div class="container-fluid content">
         <div class="bg-light m-2 p-2 show_predict">
-        <div class="user-select-none m-3">ФИО: {{$analyze->patient->name}}</div>
-        <div class="user-select-none m-3">Дата анализа: {{ date('d.m.Y H:m:s', strtotime($analyze->created_at))}}</div>
-        <div class="user-select-none m-3">Результат анализа: {{$analyze->predict_photo}}</div>
-        <div class="user-select-none m-3">Количество зубов с кариесом: {{$analyze->caries_count}}</div>
-        <div class="user-select-none m-3">Ссылка для скачивания: {{$analyze->predict_xml}}</div>
+            <div class="user-select-none m-3"><strong>Результат анализа:</strong><img src="{{$analyze->predict_photo}}"  style="float: right; width:256px; height:256px"></div>
+            <div class="user-select-none m-3"><strong>ФИО:</strong> {{$analyze->patient->name}}</div>
+            <div class="user-select-none m-3"><strong>Дата анализа:</strong> {{ date('d.m.Y H:m:s', strtotime($analyze->created_at))}}</div>
+            <div class="user-select-none m-3"><strong>Количество распознанных зубов:</strong> {{$analyze->count}}</div>
+            <div class="user-select-none m-3"><strong>Количество зубов с кариесом:</strong> {{$analyze->caries_count}}</div>
+
         @if ($analyze->user_id == Auth::id())
             <form class="d-inline" action="{{ route('analyzes.destroy', $analyze->id) }}" method="POST">
                 @csrf
