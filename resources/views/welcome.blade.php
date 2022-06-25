d25a87c2e56b0ee73292fd2eb27c94be7f27a6ae
Даниил Хижняков <khizhnyakovdd@gmail.com>
2022-06-25 23:40:34 +0500

    fix language

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <div class="container-fluid content d-flex flex-row">
         <div class="info m-4">
             <h1 class="m-3 user-select-none">SmartTeeth: на страже зубов</h1>
-            <h4 class="m-3 user-select-none">Интелектуальная технология распознования стоматологических заболеваний у детей</h4>
+            <h4 class="m-3 user-select-none">Интеллектуальная технология распознавания стоматологических заболеваний у детей</h4>
             <button class="btn btn-info m-3 to_analysis" type="button" onclick="window.location.href='/home'">Начать диагностику</button>
         </div>
         <img src="foto/5507734.svg" class="img-fluid d-none d-lg-block  teeth">
