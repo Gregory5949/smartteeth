03d38e31bc47ecd3815c2f5440d68791cdce444f
Даниил Хижняков <khizhnyakovdd@gmail.com>
2022-06-25 22:24:25 +0500

    info.blade done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,7 @@
                             <h5 class="modal-title" id="exampleModalLabel">Персональный токен</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
+                        <div class="modal-body text-break">
                         </div>
                     </div>
                 </div>
--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -1,7 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid content">
-
+    <div class="container-fluid content bg-light d-flex text-center">
+        <div class=" container-fluid">
+        <div class="row text-center">
+            <div class="col m-3">
+                <img src="/foto/s.png">
+                <h3>Владислав Сайфулин</h3>
+            </div>
+            <div class="col m-3">
+                <img src="/foto/kh.png">
+                <h3>Даниил Хижняков</h3>
+            </div>
+        </div>
+        <div class="row text-center" >
+            <div class="col m-3">
+                <img src="/foto/n.png">
+                <h3>Тимур Низамов</h3>
+            </div>
+            <div class="col m-3">
+                <img src="/foto/d.png">
+                <h3>Григорий Дрожащих</h3>
+            </div>
+        </div>
+        </div>
     </div>
 @endsection
